f6b05c3924559bf970079cc3e1bbab4a83a806ba
jwedemeyer <jwedemeyer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-02-12 17:30:07 +0000

    Test commit
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@4 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

---
+++ Brogue/Brogue/Brogue/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//THIS IS A TEST CLASS TO ENSURE MY COMMITS ARE WORKING
+//Jake
+namespace Brogue
+{
+    class Enemy
+    {
+    }
+}
