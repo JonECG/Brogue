c5d907059c69781a165a8f97881b1f031b220572
Drew Hurdle <dmahurdle@gmail.com>
2014-02-13 17:48:05 +0000

    Engine folder by Drew for Drew.
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@19 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

---
+++ Brogue/Brogue/Brogue/Engine/Engine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brogue.Engine
+{
+    class Engine
+    {
+
+        public static void Start()
+        {
+            CharacterCreation();
+            GenerateLevel();
+            StartGame();
+        }
+        public static void End()
+        {
+            
+        }
+
+        public static void CharacterCreation()
+        {
+
+        }
+
+        public static void GenerateLevel()
+        {
+
+        }
+
+        public static void StartGame()
+        {
+
+        }
+
+    }
+}
