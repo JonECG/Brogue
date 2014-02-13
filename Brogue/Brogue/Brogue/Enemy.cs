3ac5bb284973a33e1edf837b3252a66dd3ae7580
jwedemeyer <jwedemeyer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-02-13 22:15:35 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@21 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/Enemy.cs
+++ Brogue/Brogue/Brogue/Enemy.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-//THIS IS A TEST CLASS TO ENSURE MY COMMITS ARE WORKING
-//Jake
+//owned by Jake
 namespace Brogue
 {
     abstract class Enemy : GameCharacter
     {
+        //This method will be called each turn to determine who (if anyone) to attack
+        public abstract void aggro();
 
+        //This method accepts an int i (maybe change to an enum later, talk about it with you guys) which corresponds
+        //to the level of difficulty the enemy should be. This will also affect drop table choice.
+        public abstract void buildEnemy(int i);
     }
 }
