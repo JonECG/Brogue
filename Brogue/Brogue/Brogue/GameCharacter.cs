a64e127c3804c9a713652d67c331e03415522afd
Drew Hurdle <dmahurdle@gmail.com>
2014-02-14 17:01:23 +0000

    Intvec
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@34 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/GameCharacter.cs
+++ Brogue/Brogue/Brogue/GameCharacter.cs
@@ -5,44 +5,11 @@ using System.Text;
 
 namespace Brogue
 {
-    public class GridLocation
-    {
-        public int[] ints = new int[2];
-
-        public int X
-        {
-            get
-            {
-                return ints[0];
-            }
-            set
-            {
-                ints[0] = value;
-            }
-        }
-        public int Y
-        {
-            get
-            {
-                return ints[1];
-            }
-            set
-            {
-                ints[1] = value;
-            }
-        }
-        
-        public GridLocation(int x, int y)
-        {
-            ints[0] = x;
-            ints[1] = y;
-        }
-    }
+    
 
     abstract class GameCharacter
     {
         public GridLocation position;
         abstract public void TakeTurn();
-
     }
 }
---
+++ Brogue/Brogue/Brogue/IntVec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brogue
+{
+    public class IntVec
+    {
+        public int[] ints = new int[2];
+
+        public int X
+        {
+            get
+            {
+                return ints[0];
+            }
+            set
+            {
+                ints[0] = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return ints[1];
+            }
+            set
+            {
+                ints[1] = value;
+            }
+        }
+
+        public GridLocation(int x, int y)
+        {
+            ints[0] = x;
+            ints[1] = y;
+        }
+    }
+}
