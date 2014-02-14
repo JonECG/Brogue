38986730918a144cc7d6270f51a4a72797e32c77
Jonathan Pearl <jonathantpearl@gmail.com>
2014-02-14 17:23:25 +0000

    Added Direction
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@42 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

---
+++ Brogue/Brogue/Brogue/Engine/Direction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brogue.Engine
+{
+    class Direction
+    {
+        public static readonly Direction UP = new Direction(0, -1);
+        public static readonly Direction DOWN = new Direction(0, 1);
+        public static readonly Direction LEFT = new Direction(-1, 0);
+        public static readonly Direction RIGHT = new Direction(1, 0);
+
+        public static IEnumerable<Direction> Values
+        {
+            get
+            {
+                yield return UP;
+                yield return DOWN;
+                yield return LEFT;
+                yield return RIGHT;
+            }
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        private Direction(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
