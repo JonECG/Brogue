90fc144954380637d519d5822b06ea6a0dc6a233
Drew Hurdle <dmahurdle@gmail.com>
2014-02-14 16:32:35 +0000

    New gridloc
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@26 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/GameCharacter.cs
+++ Brogue/Brogue/Brogue/GameCharacter.cs
@@ -5,9 +5,33 @@ using System.Text;
 
 namespace Brogue
 {
-    struct GridLocation
+    class GridLocation
     {
         public int[] ints = new int[2];
+
+        public int X
+        {
+            get
+            {
+                return ints[0];
+            }
+            set
+            {
+                ints[0] = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return ints[1];
+            }
+            set
+            {
+                ints[1] = value;
+            }
+        }
+        
         public GridLocation(int x, int y)
         {
             ints[0] = x;
