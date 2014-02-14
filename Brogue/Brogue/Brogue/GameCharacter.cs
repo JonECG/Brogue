c3ffdca98a490a9e5c053a65d4f7a9d76038e6e2
Drew Hurdle <dmahurdle@gmail.com>
2014-02-14 16:26:13 +0000

    GridLocationStruct
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@25 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/GameCharacter.cs
+++ Brogue/Brogue/Brogue/GameCharacter.cs
@@ -5,7 +5,20 @@ using System.Text;
 
 namespace Brogue
 {
+    struct GridLocation
+    {
+        public int[] ints = new int[2];
+        public GridLocation(int x, int y)
+        {
+            ints[0] = x;
+            ints[1] = y;
+        }
+    }
+
     abstract class GameCharacter
     {
+        abstract public GridLocation position;
+        abstract public void TakeTurn();
+
     }
 }
