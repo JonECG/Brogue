502456e8fc8413fafda6ee4dd1ce5ea215f968e7
jwedemeyer <jwedemeyer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-02-17 21:39:04 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@63 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/GameCharacter.cs
+++ Brogue/Brogue/Brogue/GameCharacter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Brogue.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Brogue
     public abstract class GameCharacter
     {
 		public IntVec position;
-        abstract public void TakeTurn();
+        abstract public void TakeTurn(Level level);
         public abstract void takeDamage(int damage);
     }
 }
