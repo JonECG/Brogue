83246e08769d86bcecb823f1fe910115eaccc1d7
jwedemeyer <jwedemeyer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-02-20 17:22:02 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@108 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/GameCharacter.cs
+++ Brogue/Brogue/Brogue/GameCharacter.cs
@@ -8,11 +8,15 @@ namespace Brogue
 {
     public abstract class GameCharacter : IRenderable
     {
+        protected int health;
 		public IntVec position;
         public bool isFriendly;
         abstract public void TakeTurn(Level level);
         public abstract void TakeDamage(int damage, GameCharacter attacker);
-        public abstract void Heal(int heal);
+        public void Heal(int heal)
+        {
+            health += heal;
+        }
         public Sprite GetSprite()
         {
             return new Sprite(Tile.tileset);
