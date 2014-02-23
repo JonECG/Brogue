fd33b1c58715d725ecd078529f3d283b7df05527
Drew Hurdle <dmahurdle@gmail.com>
2014-02-23 04:39:56 +0000

    Changed light to ILightSource.
    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@174 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

---
+++ Brogue/Brogue/Brogue/ILightSource.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brogue
+{
+    public interface ILightSource : IRenderable
+    {
+        float Intensity { get; set; }
+        int MyProperty { get; set; }
+        Color Color { get; set; }
+        int FlickerWait { get; set; }
+    }
+}
