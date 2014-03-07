52004c5ac43a9a6fb66c15439c32e06667dea48d
bauxer <bauxer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-03-07 17:18:59 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@408 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/EnviromentObjects/Interactive/Gate.cs
+++ Brogue/Brogue/Brogue/EnviromentObjects/Interactive/Gate.cs
@@ -54,7 +54,6 @@ namespace Brogue.EnviromentObjects.Interactive
 
         public Sprite GetSprite()
         {
-            //return new Sprite(texture);
             return new Sprite(texture, directionFacing);
         }
 
