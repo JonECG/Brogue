707aabd56b320817bb5cbab725d0d87cf77d8a24
jwedemeyer <jwedemeyer@ffc9af0e-085f-dd4a-a650-6969ebe09bdb>
2014-03-13 15:42:01 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/brogueii_shadowwarrior@555 ffc9af0e-085f-dd4a-a650-6969ebe09bdb

--- Brogue/Brogue/Brogue/Enemies/GuardianEnemy.cs
+++ Brogue/Brogue/Brogue/Enemies/GuardianEnemy.cs
@@ -71,7 +71,7 @@ namespace Brogue.Enemies
 
             range = 1;
             defense = 15 + (5 * i);
-            attack = 5 + (5 * (i-2));
+            attack = 5 + (5 * i);
             health = 30 + (15 * i);
             exp = 50 + 25 * i-1;
         }
--- Brogue/Brogue/Brogue/Enemies/RangedEnemy.cs
+++ Brogue/Brogue/Brogue/Enemies/RangedEnemy.cs
@@ -133,7 +133,7 @@ namespace Brogue.Enemies
             defense = 0 + (3 * i);
             if (defense > 50)
                 defense = 50;
-            attack = 3 + (4 * (i-2));
+            attack = 3 + (4 * i);
             health = 30 + (10 * i);
             exp = 30 + 15 * i-1;
         }
